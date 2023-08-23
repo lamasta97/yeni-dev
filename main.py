11243eda2012c75398f2ef4a978c0cc8cdb747a4
lamasta97 <71608426+lamasta97@users.noreply.github.com>
2023-08-23 09:56:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,32 +10,28 @@ const input_workbook = xlsx.readFile(input_file);
 const input_sheet = input_workbook.Sheets[input_workbook.SheetNames[0]];
 
 // Çıktı verilerini tutacak dizi
-const output_data = [];
+const output_data = new Set(); // Using a Set to avoid duplicate keys
 
 // Tüm hücreleri gez ve verileri JSON olarak yükle
 for (const cellAddress in input_sheet) {
     if (!input_sheet.hasOwnProperty(cellAddress)) continue;
-    
+
     const cell = input_sheet[cellAddress];
     const value = cell.v;
-    
-    if (value) {
-        try {
-            const data = JSON.parse('{' + value + '}'); // JSON olarak yükle
-            if (typeof data === 'object' && data !== null) {
-                for (const key in data) {
-                    output_data.push([key.trim()]); // Sadece sol tarafları al
-                }
-            }
-        } catch (error) {
-            // Hatalı JSON geç, devam et
+
+    if (value && typeof value === 'string') {
+        const regex = /"([^"]+)":/g; // Match key names between quotes
+        let match;
+        while ((match = regex.exec(value)) !== null) {
+            output_data.add(match[1]);
         }
     }
 }
 
 // Yeni bir Excel dosyası oluştur ve verileri yaz
 const output_workbook = xlsx.utils.book_new();
-const output_sheet = xlsx.utils.aoa_to_sheet(output_data);
+const output_sheet_data = Array.from(output_data).map(data => [data]);
+const output_sheet = xlsx.utils.aoa_to_sheet(output_sheet_data);
 xlsx.utils.book_append_sheet(output_workbook, output_sheet, 'Sheet1');
 
 // Çıkış Excel dosyasını kaydet
