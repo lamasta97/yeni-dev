6142e135847df93e3c5800d6c9097867b168d81d
lamasta97 <71608426+lamasta97@users.noreply.github.com>
2023-08-23 09:28:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
-import json
-import pandas as pd
+const fs = require('fs');
+const xlsx = require('xlsx');
 
-# JSON dosyasını okuma
-with open('veriler.json', 'r', encoding='utf-8') as json_file:
-    data = json.load(json_file)
+// Giriş ve çıkış dosya adları
+const input_file = 'input.xlsx';
+const output_file = 'output.xlsx';
 
-# Tüm anahtarları al
-keys = list(data.keys())
+// Giriş Excel dosyasını aç
+const input_workbook = xlsx.readFile(input_file);
+const input_sheet = input_workbook.Sheets[input_workbook.SheetNames[0]];
 
-# Anahtarları DataFrame'e aktar
-df = pd.DataFrame({'Keys': keys})
+// Çıktı verilerini tutacak dizi
+const output_data = [];
 
-# Excel dosyasına yazma
-df.to_excel('keys.xlsx', index=False)
+// Tüm hücreleri gez ve verileri JSON olarak yükle
+for (const cellAddress in input_sheet) {
+    if (!input_sheet.hasOwnProperty(cellAddress)) continue;
+    
+    const cell = input_sheet[cellAddress];
+    const value = cell.v;
+    
+    if (value) {
+        try {
+            const data = JSON.parse('{' + value + '}'); // JSON olarak yükle
+            if (typeof data === 'object' && data !== null) {
+                for (const key in data) {
+                    output_data.push(key.trim()); // Sadece sol tarafları al
+                }
+            }
+        } catch (error) {
+            // Hatalı JSON geç, devam et
+        }
+    }
+}
+
+// Yeni bir Excel dosyası oluştur ve verileri yaz
+const output_workbook = xlsx.utils.book_new();
+const output_sheet_data = output_data.map(data => [data]);
+const output_sheet = xlsx.utils.aoa_to_sheet(output_sheet_data);
+xlsx.utils.book_append_sheet(output_workbook, output_sheet, 'Sheet1');
+
+// Çıkış Excel dosyasını kaydet
+xlsx.writeFile(output_workbook, output_file);
+
+console.log('İşlem tamamlandı.');
