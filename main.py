1f43f67038a34bd2d29675ef511f60a1f59d1a35
lamasta97 <71608426+lamasta97@users.noreply.github.com>
2023-08-23 11:05:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-const fs = require('fs');
-const xlsx = require('xlsx');
+import os
 
-// Giriş ve çıkış dosya adları
-const input_file = 'input.xlsx';
-const output_file = 'output_list.txt';
+def search_text_in_directory(directory, text_to_search):
+    total_occurrences = 0
+    
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".js"):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    occurrences = content.count(text_to_search)
+                    total_occurrences += occurrences
+    
+    return total_occurrences
 
-// Giriş Excel dosyasını aç
-const input_workbook = xlsx.readFile(input_file);
-const input_sheet = input_workbook.Sheets[input_workbook.SheetNames[0]];
+def search_multiple_words_in_project(directory, words_to_search):
+    word_occurrences = {}
 
-// Çıktı verilerini tutacak dizi
-const output_data = []; // Using an array to store the keys
+    for word in words_to_search:
+        occurrences = search_text_in_directory(directory, word)
+        word_occurrences[word] = occurrences
 
-// Tüm hücreleri gez ve verileri JSON olarak yükle
-for (const cellAddress in input_sheet) {
-    if (!input_sheet.hasOwnProperty(cellAddress)) continue;
+    return word_occurrences
 
-    const cell = input_sheet[cellAddress];
-    const value = cell.v;
+directory_to_search = r"C:\Users\hmmer\newsapi-app"  # Your project directory path
+words_to_search = [
+    'news-image', 'news-title'  # Add your list of 2400 words here
+    # ... (other words)
+]
 
-    if (value && typeof value === 'string') {
-        const regex = /"([^"]+)":/g; // Match key names between quotes
-        let match;
-        while ((match = regex.exec(value)) !== null) {
-            output_data.push(`'"${match[1]}"'`); // Add key with double quotes
-        }
-    }
-}
+word_occurrences = search_multiple_words_in_project(directory_to_search, words_to_search)
 
-// Oluşturulan verileri Python list formatına dönüştür
-const python_list = '[' + output_data.join(', ') + ']';
+# Create a text file and write data to it
+text_filename = 'word_occurrences.txt'
+with open(text_filename, 'w', encoding='utf-8') as text_file:
+    for word, occurrences in word_occurrences.items():
+        text_file.write(f"'{word}' used {occurrences} times.\n")
 
-// Python list formatını dosyaya yaz
-fs.writeFileSync(output_file, python_list);
-
-console.log('İşlem tamamlandı.');
+print(f"Word occurrences saved to '{text_filename}'.")
